e550c13174767efa92bdcb8d4511db78a0b7f19f
firstboy0513 <guohua0594@sina.com>
2013-11-29 19:51:50 +0800

    Using DP(Dynamic Programming) algorithm to calculate string edit distance

--- beautyofprogramming/string_distance.c
+++ beautyofprogramming/string_distance.c
@@ -83,12 +83,57 @@ int CalculateStringDistance(const char* sa, const int paBegin, const int paEnd,
 	return (min3(d1, d2, d3) + 1);
 }
 
+/**
+* @brief Calculate string edit distance using DP(Dynamic Programming)
+* algorithm.
+*
+* @param sa string A.
+* @param saLen string A length.
+* @param sb string B.
+* @param sbLen string B length.
+*
+* @return return distance between string A and string B.
+*/
+int CalculateStringDistanceUseDP(const char* sa, const int saLen, 
+	const char* sb, const int sbLen) {
+	if (saLen < 0 || sbLen < 0) {
+		printf("[ERR] -- CalculateStringDistanceUseDP input invalid.\n");
+		return -1;
+	}
+	if (saLen == 0) {
+		return sbLen;
+	}
+	if (sbLen == 0) {
+		return saLen;
+	}
+	int i = 0, j = 0;
+	int naLen = saLen + 1, nbLen = sbLen + 1;
+	int* _dp = (int*) malloc(sizeof(int) * naLen * nbLen);
+	int* dp[10];
+	for (i = 0; i < naLen; i ++) { dp[i] = (_dp + nbLen * i); }
+	for (i = 0; i < naLen; i ++) { dp[i][0] = i; }
+	for (j = 0; j < nbLen; j ++) { dp[0][j] = j; }
+	for (i = 1; i < naLen; i ++) {
+		for (j = 1; j < nbLen; j ++) {
+			if (sa[i-1] == sb[j-1]) {
+				dp[i][j] = dp[i-1][j-1];
+			} else {
+				dp[i][j] = min3(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1;
+			}
+		}
+	}
+	int d = dp[sbLen][saLen];
+	free(_dp);
+	return d;
+}
+
 int main(int argc, const char *argv[])
 {
 	const char* sa = "abcdef";
-	const char* sb = "acdef";
-	int d = CalculateStringDistance(sa, 0, (strlen(sa)-1),
-		sb, 0, (strlen(sb)-1));
+	const char* sb = "acdefa";
+	// int d = CalculateStringDistance(sa, 0, (strlen(sa)-1),
+	// 	sb, 0, (strlen(sb)-1));
+	int d = CalculateStringDistanceUseDP(sa, strlen(sa), sb, strlen(sb));
 	printf("string distance between A(%s) and B(%s) is %d\n", 
 		sa, sb, d);
 	return 0;
