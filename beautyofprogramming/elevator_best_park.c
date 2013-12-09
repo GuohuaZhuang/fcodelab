9ba331f4c9f5d2f73eef453627785ede61aef030
firstboy0513 <guohua0594@sina.com>
2013-12-09 14:50:21 +0800

    implement compute the best k park floors in the elevator best park problem

--- beautyofprogramming/elevator_best_park.c
+++ beautyofprogramming/elevator_best_park.c
@@ -24,7 +24,7 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#include <string.h>
 
 /**
 * @brief elevator best park floor use exhaustion to calculate it.
@@ -221,6 +221,132 @@ int elevator_best_park_use_balance(const int* floor_person_number,
 	return best_floor_park;
 }
 
+/**
+* @brief best result struct use to find the best parks.
+*/
+typedef struct _BEST_RESULT {
+	int cost;  // best cost
+	int floor; // best floor
+} BEST_RESULT; 
+
+/**
+* @brief compute current best park in given park count and remain floors.
+*
+* @param floor_person_number the person number in which floor they want to go.
+* @param floor_length floor_person_number array length.
+* @param calorie the ladder get up stair to use calorie as more weight to go 
+* down stair.
+* @param best_cost best cost array point.
+* @param floor_num remain floor count.
+* @param park_count park count.
+*
+* @return the best cost ladder.
+*/
+int compute_best_park(const int* floor_person_number, const int floor_length, 
+	int calorie, BEST_RESULT** best_cost, int floor_num, int park_count) {
+	if (best_cost[floor_num][park_count].cost > 0) {
+		return best_cost[floor_num][park_count].cost;
+	}
+	int start_floor = floor_length - floor_num;
+	if (start_floor >= floor_length) return 0;
+	int i = 0, j = 0, current_cost = 0;
+	if (park_count == 0) {
+		if (start_floor == 0) return 0;
+		for (j = start_floor; j < floor_length; j ++) {
+			current_cost += floor_person_number[j] * (j - start_floor + 1);
+		}
+		return current_cost;
+	}
+	int left = start_floor, right = floor_length - park_count + 1;
+	for (i = left; i < right; i ++) { // assume i is the park floor
+		current_cost = 0;
+		int middle = (start_floor + i) / 2;
+		if (start_floor == 0) {
+			for (j = start_floor; j < i; j ++) {
+				current_cost += floor_person_number[j] * (i - j);
+			}
+		} else {
+			for (j = start_floor; j <= middle; j ++) {
+				current_cost += floor_person_number[j] * (j - start_floor + 1);
+			}
+			for (j = middle + 1; j < i; j ++) {
+				current_cost += floor_person_number[j] * (i - j);
+			}
+		}
+		if (park_count > 0) {
+			current_cost += compute_best_park(floor_person_number, floor_length,
+				calorie, best_cost, floor_length-i-1, park_count-1);
+		}
+		if (-1 == best_cost[floor_num][park_count].cost 
+			|| current_cost < best_cost[floor_num][park_count].cost) {
+			best_cost[floor_num][park_count].cost = current_cost;
+			best_cost[floor_num][park_count].floor = i;
+		}
+	}
+	return best_cost[floor_num][park_count].cost;
+}
+
+/**
+* @brief calculate elevator best parks.
+* It using dynamic programming to store result.
+*
+* @param floor_person_number the person number in which floor they want to go.
+* @param floor_length floor_person_number array length.
+* @param pbest_floor_ladder output best floor ladder cost.
+* @param calorie the ladder get up stair to use calorie as more weight to go 
+* down stair.
+* @param park_count the count can be park.
+* @param best_parks output best parks in a int array.
+*
+* @return return 0 is ok, or return -1 means error occur.
+*/
+int* elevator_best_parks(const int* floor_person_number, 
+	const int floor_length, int* pbest_floor_ladder, int calorie, 
+	int park_count) {
+	if (!floor_person_number || floor_length <= 0) {
+		printf("[ERR] -- elevator best park invalid input.\n");
+		return NULL;
+	}
+	// initialize best result
+	int i = 0, j = 0;
+	BEST_RESULT** best_cost = (BEST_RESULT**) malloc(
+		sizeof(BEST_RESULT*) * (floor_length+1));
+	for (i = 0; i <=floor_length; i ++) {
+		best_cost[i] = (BEST_RESULT*) 
+			malloc(sizeof(BEST_RESULT) * (park_count+1));
+		for (j = 0; j <= park_count; j ++) {
+			best_cost[i][j].cost = best_cost[i][j].floor = -1;
+		}
+	}
+
+	// compute the best park
+	compute_best_park(floor_person_number, floor_length, calorie, best_cost, 
+		floor_length, park_count);
+	// debug output
+	for (i = 1; i <= floor_length; i ++) {
+		for (j = 1; j <= park_count; j ++) {
+			printf("best_cost[%d][%d] = %d, floor = %d\n", i, j, 
+				best_cost[i][j].cost, best_cost[i][j].floor);
+		}
+	}
+	int* best_parks = (int*) malloc(sizeof(int) * park_count);
+	memset(best_parks, 0, sizeof(int) * park_count);
+	int floor = floor_length;
+	while (park_count > 0) {
+		best_parks[park_count - 1] = best_cost[floor][park_count].floor;
+		floor = best_cost[floor][park_count].floor + 1;
+		park_count --;
+	}
+
+	// release memory
+	for (i = 0; i <= floor_length; i ++) {
+		free(best_cost[i]);
+	}
+	free(best_cost);
+
+	return best_parks;
+}
+
 int main(int argc, const char *argv[])
 {
 	int floor_person_number[] = {0, 2, 3, 2, 9, 2, 1, 7, 2, 4, 2};
@@ -234,10 +360,25 @@ int main(int argc, const char *argv[])
 //	int best_floor_park = elevator_best_park_use_median(floor_person_number, 
 //		sizeof(floor_person_number)/sizeof(floor_person_number[0]), 
 //		&best_floor_ladder, 2);
-	int best_floor_park = elevator_best_park_use_balance(floor_person_number, 
+//	int best_floor_park = elevator_best_park_use_balance(floor_person_number, 
+//		sizeof(floor_person_number)/sizeof(floor_person_number[0]), 
+//		&best_floor_ladder, 2);
+//	printf("best floor to park is %d, and its ladder cost %d\n", 
+//		best_floor_park+1, best_floor_ladder);
+
+	// best k parks
+	int park_count = 2;
+	int* best_parks = elevator_best_parks(floor_person_number, 
 		sizeof(floor_person_number)/sizeof(floor_person_number[0]), 
-		&best_floor_ladder, 2);
-	printf("best floor to park is %d, and its ladder cost %d\n", 
-		best_floor_park+1, best_floor_ladder);
+		&best_floor_ladder, 1, park_count);
+	printf("best k floor to park ladder cost %d, and the best parks are:\n", 
+		best_floor_ladder);
+	int i = 0;
+	for (i = 0; i < park_count; i ++) {
+		if (0 != i) printf(", ");
+		printf("%d", (best_parks[i]+1));
+	}
+	printf("\n");
+	free(best_parks);
 	return 0;
 }
