8dd609f0a90e435f7a420951bb3590561a39e998
firstboy0513 <guohua0594@sina.com>
2013-12-10 15:44:27 +0800

    use back-accumulate method to solve the split array summation to two half child array

--- beautyofprogramming/split_array_summation.cpp
+++ beautyofprogramming/split_array_summation.cpp
@@ -19,7 +19,7 @@
 * @file split_array_summation.cpp
 * @brief split array summation use accumulate method.
 * @author firstboy0513
-* @version 0.0.2
+* @version 0.0.3
 * @date 2013-12-10
 */
 #include <iostream>
@@ -86,6 +86,14 @@ int* get_summation_split(const int* array, const int length, int summation) {
 
 /**
 * @brief split array summation use accumulate method.
+* heap[0] to heap[half_length] means have the count number to summation.
+* Like this:
+* heap[0] = {empty}
+* heap[1] = {(1), (5), (7)}
+* heap[2] = {(1,5), (1,7), (5,7)}
+* heap[3] = {(1,5,7),...}
+* first initiate heap[0] = {empty}, the add (1) + empty to heap[1], 
+* then add (5) + {empty} to heap[1] and (5) + (1) to heap[2], and so on.
 *
 * @param array array.
 * @param length array length.
@@ -124,6 +132,55 @@ int* split_array_summation_use_accumulate(const int* array, const int length) {
 }
 
 /**
+* @brief split array summation use back accumulate.
+* This method is just back accumulate.Like this:
+* heap[0] = {1, 0, 0, 0, .... } // another 0 is v-1 times.
+* then want to find if(v-array[i]) is ok which be set as 1.
+* we found heap[1-0][v-1] when v=1 is ok, because heap[0][0] = 1, 
+* so set heap[1][1] = 1.
+* also, we found heap[1-0][v-5] when v=5 is ok, heap[1-0][v-7] when v=7 is ok,
+* so set heap[1][5] = 1, and heap[1][7] = 1.
+* also, we found heap[2-1][v-1] when v=6 is ok, because heap[1][5] is ok now,
+* and set heap[2][6] = 1. it means the 2 floor v=1 means (6-5)=1.
+*
+* @param array array.
+* @param length array length.
+*
+* @return split flag array point.
+*/
+int* split_array_summation_use_back_accumulate(const int* array, 
+	const int length) {
+    if (!array || length <= 0 || length % 2 != 0) {
+        printf("split_array_summation input invalid!\n");
+        return NULL;
+    }
+	int i = 0, j = 0, v = 0, current_max_index = 0;
+	int half_length = length / 2, half_sum = 0, max_half_sum = INT32_MIN;
+	for (i = 0; i < length; i ++) { half_sum += array[i]; } half_sum /= 2;
+	vector< vector<int> > heap; heap.resize(half_length + 1);
+	for (i = 0; i <= half_length; i ++) { heap[i].resize(half_sum + 1); }
+	vector<int>::iterator isubheap;
+	heap[0][0] = 1;
+	for (i = 0; i < length; i ++) {
+		current_max_index = (i < half_length ? i+1 : half_length);
+		for (j = current_max_index; j > 0; j --) {
+			for (v = 1; v <= half_sum; v ++) {
+				if (v >= array[i] && heap[j - 1][v - array[i]] == 1) {
+					heap[j][v] = 1;
+				}
+			}
+		}
+	}
+	for (v = half_sum; v >= 0; v --) {
+		if (heap[half_length][v] == 1) {
+			max_half_sum = v;
+			break;
+		}
+	}
+	return get_summation_split(array, length, max_half_sum);
+}
+
+/**
 * @brief print out the split to half array result.
 *
 * @param array 2*n array.
@@ -156,7 +213,8 @@ int main(int argc, const char *argv[])
 {
 	int array[] = {1, 5, 7, 8, 9, 6, 3, 11, 20, 17};
 	int length = sizeof(array) / sizeof(array[0]);
-	int* split = split_array_summation_use_accumulate(array, length);
+	// int* split = split_array_summation_use_accumulate(array, length);
+	int* split = split_array_summation_use_back_accumulate(array, length);
 	if (split) {
 		print_result(array, length, split);
 		free(split);
