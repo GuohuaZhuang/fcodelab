1891cb1b582eaa90e8cfcfcf388e439e24bc78a7
firstboy0513 <guohua0594@sina.com>
2014-01-12 14:01:41 +0800

    add comment in the code file tetirs game

--- beautyofprogramming/tetris.c
+++ beautyofprogramming/tetris.c
@@ -174,6 +174,10 @@ BLOCK g_block_sets[BLOCK_TYPE][BLOCK_ROTATE_SIZE] = {
 	}
 };
 
+/**
+* @brief panel include TETRIS_HEIGHT*TETRIS_WIDTH table cells, also include 
+* column height named as colheight array.
+*/
 typedef struct _PANEL {
 	int panel[TETRIS_HEIGHT][TETRIS_WIDTH];
 	int colheight[TETRIS_WIDTH];
@@ -213,14 +217,12 @@ void output_all_tetris_blocks() {
 	}
 }
 
+/**
+* @brief initialize tetrix panel.
+*
+* @return return initialized tetrix panel.
+*/
 PANEL* init_tetris_panel() {
-//	int i = 0;
-//	int** panel = (int**) malloc(sizeof(int*) * TETRIS_HEIGHT);
-//	for (i = 0; i < TETRIS_HEIGHT; i ++) {
-//		panel[i] = (int*) malloc(sizeof(int) * TETRIS_WIDTH);
-//		memset(panel[i], 0, sizeof(int) * TETRIS_WIDTH);
-//	}
-//	return panel;
 	PANEL* panel = (PANEL*) malloc(sizeof(PANEL));
 	memset(panel, 0, sizeof(PANEL));
 	int i = 0;
@@ -230,14 +232,20 @@ PANEL* init_tetris_panel() {
 	return panel;
 }
 
+/**
+* @brief release tetris panel.
+*
+* @param panel tetrix panel.
+*/
 void release_tetris_panel(PANEL* panel) {
-//	int i = 0;
-//	for (i = 0; i < TETRIS_HEIGHT; i ++) {
-//		free(panel[i]);
-//	}
 	free(panel);
 }
 
+/**
+* @brief output in the console of tetris panel table cells.
+*
+* @param panel tetrix panel.
+*/
 void output_tetris_panel(PANEL* panel) {
 	int i = 0, j = 0;
 	printf("---------------------------\n");
@@ -255,6 +263,11 @@ void output_tetris_panel(PANEL* panel) {
 	printf("---------------------------\n");
 }
 
+/**
+* @brief best place include the block rotate as best_rotate, best place 
+* left-top column position as best_j, best row as best_y, and corresponding 
+* best clean line count.
+*/
 typedef struct _BEST_PLACE {
 	int best_rotate;
 	int best_j;
@@ -262,6 +275,13 @@ typedef struct _BEST_PLACE {
 	int best_cleanline;
 } BEST_PLACE;
 
+/**
+* @brief get block extreme column range.
+*
+* @param block block point.
+* @param pmin_col minimum column index.
+* @param pmax_col maximum column index.
+*/
 void get_col_extreme_range(BLOCK* block, int* pmin_col, int* pmax_col) {
 
 	assert(pmin_col != NULL);
@@ -281,6 +301,13 @@ void get_col_extreme_range(BLOCK* block, int* pmin_col, int* pmax_col) {
 	}
 }
 
+/**
+* @brief get block extreme row range.
+*
+* @param block block point.
+* @param pmin_row minimum row index.
+* @param pmax_row maximum row index.
+*/
 void get_row_extreme_range(BLOCK* block, int* pmin_row, int* pmax_row) {
 
 	assert(pmin_row != NULL);
@@ -300,6 +327,12 @@ void get_row_extreme_range(BLOCK* block, int* pmin_row, int* pmax_row) {
 	}
 }
 
+/**
+* @brief get block all column offsets.
+*
+* @param block block point.
+* @param bcol block column array.
+*/
 void get_offset_block_col(BLOCK* block, int* bcol) {
 	int i = 0, j = 0;
 	for (j = 0; j < BLOCK_SIZE; j ++) {
@@ -310,7 +343,15 @@ void get_offset_block_col(BLOCK* block, int* bcol) {
 	}
 }
 
-// OK
+/**
+* @brief get x offset range.
+*
+* @param block block point.
+* @param pmin_col minimum column index in panel.
+* @param pmax_col maximum column index in panel.
+* @param pbmin_col block itself minimum column.
+* @param pbmax_col block itself maximum column.
+*/
 void get_offset_x_range(BLOCK* block, 
 	int* pmin_col, int* pmax_col, int* pbmin_col, int* pbmax_col) {
 	*pbmin_col = 0, *pbmax_col = 0;
@@ -319,7 +360,20 @@ void get_offset_x_range(BLOCK* block,
 	*pmax_col = TETRIS_WIDTH - 1 - *pbmax_col;
 }
 
-// OK
+/**
+* @brief get bottom offset row index as y. The y value is min(colheight - bcol).
+* And colheight is the current panel maximum height, bcol is the column offset 
+* in the block itself.
+*
+* @param panel panel.
+* @param block block.
+* @param j the column of block position left index.
+* @param bmin_col block minimum column.
+* @param bmax_col block maximum column.
+* @param bcol block column offsets array.
+*
+* @return return the minimum row position as the y offset.
+*/
 int get_bottom_offset_y(PANEL* panel, BLOCK* block, int j, 
 	int bmin_col, int bmax_col, int* bcol) {
 	int i = 0, _j = 0, min_offset = TETRIS_HEIGHT, cur_offset = 0;
@@ -332,6 +386,14 @@ int get_bottom_offset_y(PANEL* panel, BLOCK* block, int j,
 	return min_offset;
 }
  
+/**
+* @brief set tetris place in panel use block.
+*
+* @param panel panel.
+* @param block block to set in panel.
+* @param place_j block in the panel column position.
+* @param place_y block in the panel row position.
+*/
 void set_tetris_place_use_block(PANEL* panel, BLOCK* block, int place_j, 
 	int place_y) {
 	int i = 0, j = 0, _i = 0, _j = 0;
@@ -348,14 +410,30 @@ void set_tetris_place_use_block(PANEL* panel, BLOCK* block, int place_j,
 		}
 	}
 }
-
-// OK
+ 
+/**
+* @brief set tetris place in panel use block rotate and block type.
+*
+* @param panel panel.
+* @param block_rotate block rotate.
+* @param block_type block type.
+* @param place_j block in the panel column position.
+* @param place_y block in the panel row position.
+*/
 void set_tetris_place(PANEL* panel, int block_rotate, int block_type, 
 	int place_j, int place_y) {
 	BLOCK* block = &(g_block_sets[block_type][block_rotate]);
 	set_tetris_place_use_block(panel, block, place_j, place_y);
 }
 
+/**
+* @brief judge the row in panel is full or not.
+*
+* @param panel panel.
+* @param y the row in panel.
+*
+* @return return 1 means the row in panel is full, otherwise is not full.
+*/
 int tetris_panel_row_is_full(PANEL* panel, int y) {
 	int _j = 0;
 	for (_j = 0; _j < TETRIS_WIDTH; _j ++) {
@@ -366,10 +444,16 @@ int tetris_panel_row_is_full(PANEL* panel, int y) {
 	return 1;
 }
 
+/**
+* @brief tetris panel cleanline set.
+*
+* @param panel panel.
+* @param y the row of block start in panel.
+*/
 void tetris_panel_cleanline(PANEL* panel, int y) {
 	int _i = 0, _j = 0;
 	for (_i = y-1; _i >= 0; _i --) {
-		memcpy(panel->panel[_i], panel->panel[_i+1], sizeof(int)*TETRIS_WIDTH);
+		memcpy(panel->panel[_i+1], panel->panel[_i], sizeof(int)*TETRIS_WIDTH);
 	}
 	for (_j = 0; _j < TETRIS_WIDTH; _j ++) {
 		if (panel->colheight[_j] < TETRIS_HEIGHT) {
@@ -378,9 +462,17 @@ void tetris_panel_cleanline(PANEL* panel, int y) {
 	}
 }
 
+/**
+* @brief tetris cleanline and retrn cleanline count.
+*
+* @param panel panel.
+* @param y the row of block start in panel.
+*
+* @return return cleanline count.
+*/
 int tetris_cleanline(PANEL* panel, int y) {
 	int _i = 0, _max_i = BLOCK_SIZE-1, cleanline = 0;
-	for (_i = 0; _i <= _max_i; _i ++) {
+	for (_i = 0; _i <= _max_i && _i+y < TETRIS_HEIGHT; _i ++) {
 		if (tetris_panel_row_is_full(panel, _i+y)) {
 			tetris_panel_cleanline(panel, _i+y);
 			cleanline ++;
@@ -390,22 +482,52 @@ int tetris_cleanline(PANEL* panel, int y) {
 	return cleanline;
 }
 
+/**
+* @brief weight score of clean line count.
+*/
 const int g_cleanline_wscore[BLOCK_SIZE] = {1, 3, 7, 13};
+/**
+* @brief weight score of great than high height.
+*/
 const int g_high_height = TETRIS_HEIGHT * 3 / 5;
 
-int tetris_holecount(PANEL* panel, int j) {
+/**
+* @brief calculate the hole count in tetris panel in given column.
+*
+* @param panel tetris panel.
+* @param col given column.
+*
+* @return return hole count in given column.
+*/
+int tetris_holecount(PANEL* panel, int col) {
 	int _i = 0, holecount = 0;
-	for (_i = panel->colheight[j]; _i < TETRIS_HEIGHT; _i ++) {
-		if (0 == panel->panel[_i][j]) {
+	for (_i = panel->colheight[col]; _i < TETRIS_HEIGHT; _i ++) {
+		if (0 == panel->panel[_i][col]) {
 			holecount ++;
 		}
 	}
 	return holecount;
 }
 
-// TODO
-double get_tetris_place_score(PANEL* panel, BLOCK* block,
-	int j, int y, int* pcleanline) {
+/**
+* @brief calculate tetris place score in one place solution.
+* The weight score rule:
+* 	1. clean line count is {1, 2, 3, 4} add corresponding {1, 3, 7, 13} score.
+* 	2. generate 1 hole subtract 4 score, when holecount > 5, subtract 15 score.
+* 	3. when place more than high height subtract 2 score every line.
+*
+* @param panel tetris panel.
+* @param block block point.
+* @param bmin_col block itself minimum column.
+* @param bmax_col block itself maximum column.
+* @param j block column position in panel.
+* @param y block row position in panel.
+* @param pcleanline output clean line count.
+*
+* @return return weight score.
+*/
+double get_tetris_place_score(PANEL* panel, BLOCK* block, 
+	int bmin_col, int bmax_col, int j, int y, int* pcleanline) {
 
 	assert(pcleanline != NULL);
 	if (y < 0) { *pcleanline = -1; return -1; }
@@ -426,7 +548,7 @@ double get_tetris_place_score(PANEL* panel, BLOCK* block,
 	y += *pcleanline;
 
 	// subtract hole count score
-	for (_j = j; _j < j+BLOCK_SIZE; _j ++) {
+	for (_j = j+bmin_col; _j <= j+bmax_col; _j ++) {
 		holecount += tetris_holecount(tmppanel, _j)-tetris_holecount(panel, _j);
 	}
 	score -= holecount*4;
@@ -442,6 +564,17 @@ double get_tetris_place_score(PANEL* panel, BLOCK* block,
 	return score;
 }
 
+/**
+* @brief recursive to place all possible position, and compare with place 
+* weight score to get the best place solution.
+* The recursion is use every 4 block rotate, and minimum column to maximum 
+* column index in panel.
+*
+* @param panel tetris panel.
+* @param block_type block type.
+*
+* @return return best place solution.
+*/
 BEST_PLACE recursion_places_and_get_best(PANEL* panel, int block_type) {
 	BEST_PLACE best_place = {0, 0, 0, 0};
 	int i = 0, j = 0, y = 0, best_wscore = INT32_MIN, wscore = 0, cleanline = 0;
@@ -454,7 +587,8 @@ BEST_PLACE recursion_places_and_get_best(PANEL* panel, int block_type) {
 		get_offset_block_col(block, bcol);
 		for (j = min_col; j <= max_col; j ++) {             // all column range
 			y = get_bottom_offset_y(panel, block, j, bmin_col, bmax_col, bcol);
-			wscore = get_tetris_place_score(panel, block, j, y, &cleanline);
+			wscore = get_tetris_place_score(panel, block, bmin_col, bmax_col, 
+				j, y, &cleanline);
 			if (best_wscore < wscore) {
 				best_wscore = wscore;
 				best_place.best_rotate = i;
@@ -468,15 +602,27 @@ BEST_PLACE recursion_places_and_get_best(PANEL* panel, int block_type) {
 	return best_place;
 }
 
+/**
+* @brief set best tetris place in the panel.
+*
+* @param panel tetris panel.
+* @param block_type block type.
+*
+* @return return best place clean line count.
+*/
 int set_best_tetris_place(PANEL* panel, int block_type) {
 	BEST_PLACE best_place = recursion_places_and_get_best(panel, block_type);
 	if (best_place.best_y >= 0) {
 		set_tetris_place(panel, best_place.best_rotate, block_type, 
 			best_place.best_j, best_place.best_y);
+		tetris_cleanline(panel, best_place.best_y);
 	}
 	return best_place.best_cleanline;
 }
 
+/**
+* @brief run tetris until game ove to break out.
+*/
 void run_tetris() {
 
 	PANEL* panel = init_tetris_panel();
@@ -499,7 +645,7 @@ void run_tetris() {
 		printf("now the score is: %d\n", score);
 
 		// just for debug
-		// break;
+		printf("press enter to go the next circle. ");
 		getchar();
 		// end for debut
 	}
