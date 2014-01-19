e77f65f412db43c4242e40fdf9800e74c69493a0
firstboy0513 <guohua0594@sina.com>
2014-01-19 11:20:25 +0800

    add delete node method in AVL tree

--- mathproblem/avl_tree.cpp
+++ mathproblem/avl_tree.cpp
@@ -80,6 +80,84 @@ int SearchAVL(BSTTree &T, ElemType e) {
 	else { return SearchAVL(T->rchild, e); }
 }
 
+void TraversalAVL(BSTTree &T, void function(BSTNode*)) {
+	if (!T) { return; }
+	if (T->lchild) { TraversalAVL(T->lchild, function); }
+	function(T);
+	if (T->rchild) { TraversalAVL(T->rchild, function); }
+}
+
+typedef struct _POINT {
+	BSTNode* p;       // point
+	struct _POINT* next; // father
+} POINT;
+
+POINT* point_push(POINT* root, BSTNode* p) {
+	POINT* new_point = (POINT*) calloc(1, sizeof(POINT));
+	new_point->p = p; new_point->next = NULL;
+	new_point->next = root;
+	root = new_point;
+	return root;
+}
+
+BSTNode* point_pop(POINT* root) {
+	if (!root) { return NULL; }
+	BSTNode* p = root->p;
+	POINT* cursor = root;
+	root = root->next;
+	free(cursor);
+	return p;
+}
+
+void Balance(BSTTree T) {
+}
+
+int DeleteAVL(BSTTree &T, ElemType e, bool &taller) {
+	if (!T) { return 0; taller = false; }
+	BSTNode* Z = NULL, *Y = NULL;
+	POINT *ZP = NULL; // Z parent node point
+	if (EQ(e, T->data)) {
+		ZP = point_push(ZP, T);
+		if (T->lchild != NULL && T->rchild != NULL) {
+			Y = T->lchild;
+			while (Y->rchild) { ZP = point_push(ZP, Y); Y = Y->rchild; Z = Y; }
+			T->data = Y->data;
+		} else { // has only one child or no child
+			Z = (NULL != T->lchild ? T->lchild : T->rchild);
+		}
+		// remove Z node
+		(Z->lchild != NULL) 
+			? ZP = point_push(ZP, Z->lchild) : ZP = point_push(ZP, Z->rchild);
+		free(Z); taller = true;
+		ZP->p->bf --;
+		if (ZP->p->bf == LH || ZP->p->bf == RH) { taller = false; }
+		else if (ZP->p->bf == 2) {
+			LeftBalance(ZP->p); if (ZP->p->bf == EH) { taller = true; }
+		} else if (ZP->p->bf == -2) {
+			RightBalance(ZP->p); if (ZP->p->bf == EH) { taller = true; }
+		} else {
+			taller = true;
+		}
+		if (ZP->p == T) { return 1; }
+		BSTNode* _tmp = NULL;
+		do {
+			_tmp = point_pop(ZP);
+			Balance(_tmp);
+		} while (_tmp != T);
+	} else if (LT(e, T->data)) {
+		int ret = DeleteAVL(T->lchild, e, taller);
+		if (!ret) { return 0; }
+		if (!taller) { return 1; }
+		/// balance one ...
+	} else {
+		int ret = DeleteAVL(T->rchild, e, taller);
+		if (!ret) { return 0; }
+		if (!taller) { return 1; }
+		/// balance one ...
+	}
+	return 1;
+}
+
 int InsertAVL(BSTTree &T, ElemType e, bool &taller) {
 	if (!T) {
 		T = (BSTTree) malloc(sizeof(BSTNode));
@@ -116,6 +194,8 @@ int InsertAVL(BSTTree &T, ElemType e, bool &taller) {
 	return 1;
 }
 
+void printout_avl_node(BSTNode* p) { printf("%d ", p->data); }
+
 int main(int argc, const char *argv[])
 {
 	BSTTree T = NULL;
@@ -125,6 +205,10 @@ int main(int argc, const char *argv[])
 	printf("insert ret = %d\n", InsertAVL(T, 32, taller));
 	printf("search ret = %d\n", SearchAVL(T, 32));
 	printf("search ret = %d\n", SearchAVL(T, 33));
+
+	printf("TraversalAVL:\n");
+	TraversalAVL(T, printout_avl_node);
+	printf("\n");
 
 	return 0;
 }
