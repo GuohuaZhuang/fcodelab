3ade3758c07255a77fb28f1105d949f022f67d18
firstboy0513 <guohua0594@sina.com>
2014-01-20 15:59:07 +0800

    fix bug in avl delete node and avl insert node in avl tree

--- mathproblem/avl_tree.c
+++ mathproblem/avl_tree.c
@@ -1,3 +1,31 @@
+/* Copyright (C) 
+* 2014 - firstboy0513
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+* 
+*/
+/**
+* @file avl_tree.c
+* @brief AVL tree, also named as balance binary search/sort tree, implement in 
+* C program.
+* reference:
+* 	http://en.wikipedia.org/wiki/AVL_tree
+* 	http://zh.wikipedia.org/wiki/AVL%E6%A0%91
+* @author firstboy0513
+* @version 0.0.1
+* @date 2014-01-20
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -72,6 +100,8 @@ PRIVATE void _avl_lr_rotate(TREE* pT) {
 	// update balance factor
 	if (lrchild->bf == LH) {
 		(*pT)->bf = EH; (*pT)->lchild->bf = EH; (*pT)->rchild->bf = RH;
+	} else if (lrchild->bf == EH) { // this case may happen as h-3==0 situation
+		(*pT)->bf = EH; (*pT)->lchild->bf = EH; (*pT)->rchild->bf = EH;
 	} else { // lrchild->bf == RH
 		(*pT)->bf = EH; (*pT)->lchild->bf = LH; (*pT)->rchild->bf = EH;
 	}
@@ -98,6 +128,8 @@ PRIVATE void _avl_rl_rotate(TREE* pT) {
 	// update balance factor
 	if (rlchild->bf == LH) {
 		(*pT)->bf = EH; (*pT)->lchild->bf = EH; (*pT)->rchild->bf = RH;
+	} else if (rlchild->bf == EH) { // this case may happen as h-3==0 situation
+		(*pT)->bf = EH; (*pT)->lchild->bf = EH; (*pT)->rchild->bf = EH;
 	} else { // rlchild->bf == RH
 		(*pT)->bf = EH; (*pT)->lchild->bf = LH; (*pT)->rchild->bf = EH;
 	}
@@ -195,12 +227,13 @@ PRIVATE void _avl_deletenode(TREE* pT, int* plower) {
 		NODE* Y = (*pT)->lchild;
 		if (NULL == Y->rchild) {
 			ELEMENT_COPY((*pT)->data, Y->data);
-			(*pT) = Y->lchild; (*plower) = 1; free(Y);
+			(*pT)->lchild = Y->lchild; (*plower) = 1; free(Y);
 			_avl_left_lower_rebalance(pT, plower);
 		} else { // NULL != Y->rchild
 			TREE_STACK* TS = NULL;
+			tree_stack_push(&TS, &((*pT)->lchild));
 			while (Y->rchild) {
-				tree_stack_push(&TS, &Y);
+				if (Y->rchild->rchild) { tree_stack_push(&TS, &(Y->rchild)); }
 				Y = Y->rchild;
 			}
 			/// copy data to pT node and delete Y node
@@ -364,22 +397,49 @@ PUBLIC void avl_traveral(TREE T, void function(NODE*)) {
 
 void printout_node(NODE* p) { printf("%d ", p->data); }
 
-int main(int argc, const char *argv[])
-{
+void testcase_for_random() {
 	TREE T = avl_init();
 	int taller = 0, lower = 0;
-
-	printf("avl_insert(T, 32) = %d\n", avl_insert(&T, 32, &taller));
-
-	printf("avl_search(T, 32) = %d\n", avl_search(T, 32));
-
-	printf("avl_delete(T, 32) = %d\n", avl_delete(&T, 32, &lower));
-
+	int i = 0, n = 100;
+	int _t = 0;
+	for (i = 0; i < n; i ++) {
+		_t = rand() % 100;
+		printf("avl_insert(&T, %d) = %d\n", _t, avl_insert(&T, _t, &taller));
+	}
+	for (i = 0; i < 20; i ++) {
+		_t = rand() % 100;
+		printf("avl_search(T, %d) = %d\n", _t, avl_search(T, _t));
+	}
 	printf("avl_traveral: ");
 	avl_traveral(T, printout_node);
 	printf("\n");
-
+	for (i = 0; i < 20; i ++) {
+		_t = rand() % 100;
+		printf("avl_delete(&T, %d) = %d\n", _t, avl_delete(&T, _t, &lower));
+	}
+	printf("avl_traveral: ");
+	avl_traveral(T, printout_node);
+	printf("\n");
 	avl_destory(T);
+}
+
+int main(int argc, const char *argv[])
+{
+	testcase_for_random();
+//	TREE T = avl_init();
+//	int taller = 0, lower = 0;
+//
+//	printf("avl_insert(T, 32) = %d\n", avl_insert(&T, 32, &taller));
+//
+//	printf("avl_search(T, 32) = %d\n", avl_search(T, 32));
+//
+//	printf("avl_delete(T, 32) = %d\n", avl_delete(&T, 32, &lower));
+//
+//	printf("avl_traveral: ");
+//	avl_traveral(T, printout_node);
+//	printf("\n");
+//
+//	avl_destory(T);
 
 	return 0;
 }
