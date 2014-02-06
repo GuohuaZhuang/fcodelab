08bf23b145d9c00ac8ee793f97362e8bd503edb0
firstboy0513 <guohua0594@sina.com>
2014-02-06 11:21:29 +0800

    add split child node code in B tree program

--- mathproblem/b_tree.c
+++ mathproblem/b_tree.c
@@ -24,12 +24,19 @@
 */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * @brief B tree public method extern to use or private be invoke.
 */
 #define PUBLIC
 #define PRIVATE static
+
+#define NIL		NULL
+
+#define BT		4
+#define FULL_KEY_COUNT (2*BT-1)
+#define FULL_CHILD_COUNT (2*BT)
 
 /**
 * @brief Element compare EQ as equal, LT as little than, GT as greate than.
@@ -62,12 +69,12 @@ typedef enum { FALSE, TRUE } BOOL;
 * point which is parent node point, left child and right child node point.
 */
 typedef struct _NODE {
-	ELEMENT data;
-	struct _NODE* p;
-	struct _NODE* left;
-	struct _NODE* right;
+///	ELEMENT data;
+///	struct _NODE* p;
 	int n;
 	BOOL leaf;
+	ELEMENT* key;
+	struct _NODE** c;
 } NODE;
 
 /**
@@ -88,13 +95,20 @@ int btree_insert(TREE* T, ELEMENT d);
 int btree_delete(TREE* T, ELEMENT d);
 void btree_traversal(TREE* T, void function(NODE*));
 
+// OK
 /**
 * @brief Internal methods of B-Tree structure.
 */
-NODE* _btree_allocate_node();
-void _btree_disk_write(NODE* x);
+NODE* _btree_allocate_node() {
+	NODE* x = (NODE*) malloc(sizeof(NODE));
+	x->c = (struct _NODE**) malloc(sizeof(struct _NODE*) * FULL_CHILD_COUNT);
+	x->key = (ELEMENT*) malloc(sizeof(ELEMENT) * FULL_KEY_COUNT);
+	memset(x->c, 0, sizeof(struct _NODE*) * FULL_CHILD_COUNT);
+	memset(x->key, 0, sizeof(struct _NODE*) * FULL_KEY_COUNT);
+	return x;
+}
 
-void _btree_init(TREE* T) {
+void _btree_create(TREE* T) {
 	NODE* x = _btree_allocate_node();
 	x->leaf = TRUE;
 	x->n = 0;
@@ -103,7 +117,12 @@ void _btree_init(TREE* T) {
 }
 
 // TODO
-void _btree_destory_node() {
+void _btree_destory_node(NODE* x) {
+	int i = 0;
+	for (i = 0; i <= x->n; i ++) {
+		_btree_destory_node(x->c[i]);
+	}
+	free(x);
 }
 
 // TODO
@@ -113,13 +132,72 @@ void btree_destory(TREE* T) {
 }
 
 // TODO
-int btree_search(TREE* T, ELEMENT d) {
-	return -132423;
+void _btree_disk_read(NODE* x) {
 }
 
 // TODO
-int btree_insert(TREE* T, ELEMENT d) {
-	return -132423;
+void _btree_disk_write(NODE* x) {
+}
+
+// OK
+int _btree_search(NODE* x, ELEMENT k, NODE** px, int* pi) {
+	int i = 0;
+	while (i <= x->n && k < x->key[i]) { i ++; }
+	if (i <= x->n && k == x->key[i]) {
+		*px = x; *pi = i;
+		return 1;
+	} else if (x->leaf) {
+		return 0;
+	} else {
+		_btree_disk_read(x->c[i]);
+		return _btree_search(x->c[i], k);
+	}
+}
+
+// OK
+int btree_search(TREE* T, ELEMENT k) {
+	NODE* x = NIL;
+	int i = 0;
+	return _btree_search(T->root, k, &x, &i);
+}
+
+// OK
+void _btree_split_child(NODE* x, int i) {
+	NODE* z = _btree_allocate_node();
+	NODE* y = x->c[i];
+	z->leaf = y->leaf;
+	z->n = BT-1;
+	int j = 0;
+	for (j = 0; j <= BT-1; j ++) {
+		z->key[j] = y->key[j+BT];
+	}
+	if (!y->leaf) {
+		for (j = 0; j <= BT; j ++) {
+			z->c[j] = y->c[j+BT];
+		}
+	}
+	y->n = BT-1;
+	for (j = x->n+1; j >= i+1; j --) {
+		x->c[j+1] = x->c[j];
+	}
+	x->c[i+1] = z;
+	for (j = x->n; j >= i; j --) {
+		x->key[j+1] = x->key[j];
+	}
+	x->key[i] = y->key[i];
+	x->n ++;
+	_btree_disk_write(y);
+	_btree_disk_write(z);
+	_btree_disk_write(x);
+}
+
+// TODO
+// It's some more complicted here...
+void btree_insert(TREE* T, ELEMENT d) {
+	NODE* r = T->root;
+	if (r->n == FULL_KEY_COUNT) {
+	} else {
+	}
 }
 
 // TODO
