d0c8c0112ce105b2ac3e9ab36390db2e575f68d8
firstboy0513 <guohua0594@sina.com>
2014-02-12 18:16:45 +0800

    implement B+ tree insert method

--- mathproblem/b_plus_tree.c
+++ mathproblem/b_plus_tree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 /**
  * @brief B+ tree public method extern to use or private be invoke.
@@ -11,8 +12,8 @@
 #define NIL		NULL
 
 #define ORDER_B				4
-#define FULL_KEY_COUNT 		(ORDER_B-1)
-#define FULL_CHILD_COUNT 	(ORDER_B)
+#define FULL_KEY_COUNT 		(ORDER_B)
+#define FULL_CHILD_COUNT 	(ORDER_B+1)
 
 /**
 * @brief Element compare EQ as equal, LT as little than, GT as greate than.
@@ -79,7 +80,7 @@ typedef struct _SEARCH_RET {
 */
 typedef struct _TREE {
 	struct _NODE* root;
-	struct _NODE* leftmost;
+///	struct _NODE* leftmost;
 } TREE;
 
 /**
@@ -102,12 +103,13 @@ PRIVATE void _bptree_create(TREE* T);
 PRIVATE void _bptree_free_node(NODE* x);
 PRIVATE void _bptree_destory_node(NODE* x);
 PRIVATE SEARCH_RET _bptree_search(NODE* x, ELEMENT k);
-/// PRIVATE void _bptree_split_child(NODE* x, int i);
+PRIVATE void _bptree_split_child(NODE* x, int i);
 /// PRIVATE void _bptree_merge_child(NODE* x, int i);
-/// PRIVATE void _bptree_insert_nonfull(NODE* x, ELEMENT k);
+PRIVATE void _bptree_insert_nonfull(NODE* x, ELEMENT k);
 /// PRIVATE void _bptree_delete_balance(NODE* x, int i);
 /// PRIVATE void _bptree_delete(NODE* x, ELEMENT k);
-/// PRIVATE void _bptree_traveral(NODE* x, void function(ELEMENT));
+PRIVATE void _bptree_traveral(NODE* x, void function(ELEMENT));
+PRIVATE NODE* _bptree_leftmost(TREE* T);
 
 // TODO
 PRIVATE void _bptree_disk_read(NODE* x) {
@@ -197,8 +199,8 @@ PUBLIC void bptree_destory(TREE* T) {
 
 /**
 * @brief B+ tree internal search.
-* Keys may be duplicated; Every key to the right of a particular key is > to 
-* that key, and corresponding the left of a particular key is <= to that key.
+* Keys may be duplicated; Every key to the right of a particular key is >= to 
+* that key, and corresponding the left of a particular key is < to that key.
 *
 * @param x subtree root.
 * @param k the element to search.
@@ -216,6 +218,7 @@ PRIVATE SEARCH_RET _bptree_search(NODE* x, ELEMENT k) {
 			return SEARCH_NIL;
 		}
 	} else {
+		if (i < x->n && EQ(k, x->key[i])) { i ++; } // equal case go right
 		_bptree_disk_read(x->c[i]);
 		return _bptree_search(x->c[i], k);
 	}
@@ -239,6 +242,102 @@ PUBLIC int bptree_search(TREE* T, ELEMENT k) {
 	} else {
 		return 1;
 	}
+}
+
+/**
+* @brief B+ tree split child.
+* Require the x->c[i] child node has full keys.
+*
+* @param x x node's i child.
+* @param i index of the child pointer.
+*/
+PRIVATE void _bptree_split_child(NODE* x, int i) {
+	int j = 0, L_H = (int)ceil((double)ORDER_B/2), R_H = ORDER_B - L_H;
+	NODE* z = _bptree_allocate_node();
+	NODE* y = x->c[i];
+	z->leaf = y->leaf;
+	z->n = R_H;
+	// Split x->c[i] into y and z, the y indicate x->c[i], and z indicate the 
+	// newer allocate node.
+	// if (y is a leaf node) {
+	//   1. move end R_H of y key[] and c[] into z, and update sequence pointer.
+	//   2. move the z->key[0] and z into x.
+	//   3. update z->n = R_H, y->n = L_H and x->n ++.
+	// } else { // y is an internal node
+	//   1. move end R_H of y key[] and c[] into z.
+	//   2. move the y->key[L_H-1] and y->c[L_H] into x.
+	//   3. update z->n = R_H, y->n = L_H-1 and x->n ++.
+	// }
+	for (j = 0; j < R_H; j ++) { ELEMENT_COPY(z->key[j], y->key[L_H+j]); }
+	for (j = 0; j <= R_H; j ++) { z->c[j] = y->c[L_H+j]; }
+	for (j = x->n; j >= i+1; j --) { x->c[j+1] = x->c[j]; }
+	for (j = x->n-1; j >= i; j --) { ELEMENT_COPY(x->key[j+1], x->key[j]); }
+	if (y->leaf) {
+		z->c[ORDER_B-1] = y->c[ORDER_B-1];
+		y->c[ORDER_B-1] = z;
+		y->n = L_H;
+		ELEMENT_COPY(x->key[i], z->key[0]);
+	} else {
+		y->n = L_H-1;
+		ELEMENT_COPY(x->key[i], y->key[L_H-1]);
+	}
+	x->c[i+1] = z;
+	x->n ++;
+
+	_bptree_disk_write(y);
+	_bptree_disk_write(z);
+	_bptree_disk_write(x);
+}
+
+/**
+* @brief B+ tree internal insert element into non-full node method.
+*
+* @param x the subtree root.
+* @param k the element to insert.
+*/
+PRIVATE void _bptree_insert_nonfull(NODE* x, ELEMENT k) {
+	int i = x->n-1;
+	if (x->leaf) {
+		while (i >= 0 && LT(k, x->key[i])) {
+			ELEMENT_COPY(x->key[i+1], x->key[i]);
+			i --;
+		}
+		ELEMENT_COPY(x->key[i+1], k);
+		x->n ++;
+		_bptree_disk_write(x);
+	} else {
+		while (i >= 0 && LT(k, x->key[i])) { i --; }
+		i ++;
+		_bptree_disk_read(x->c[i]);
+		_bptree_insert_nonfull(x->c[i], k);
+		if (ORDER_B == ((NODE*)(x->c[i]))->n) {
+			_bptree_split_child(x, i);
+		}
+	}
+}
+
+/**
+* @brief B+ tree insert method.
+*
+* @param T B+ tree.
+* @param k the element to insert.
+*
+* @return return 1 means insert success, otherwise return 0 means not insert it
+* as the element is already include in the B+ tree.
+*/
+PUBLIC int bptree_insert(TREE* T, ELEMENT k) {
+	if (bptree_search(T, k)) { return 0; }	// if duplicated is not support
+	NODE* r = T->root;
+	_bptree_insert_nonfull(r, k);
+	if (ORDER_B == r->n) {
+		NODE* s = _bptree_allocate_node();
+		T->root = s;
+		s->leaf = FALSE;
+		s->n = 0;
+		s->c[0] = r;
+		_bptree_split_child(s, 0);
+	}
+	return 1;
 }
 
 
@@ -273,13 +372,30 @@ PRIVATE void _bptree_traveral(NODE* leftmost, void function(ELEMENT)) {
 }
 
 /**
+* @brief B+ tree get leftmost node pointer.
+*
+* @param T B+ tree.
+*
+* @return return leftmost node pointer, if the tree is not exist or its root is
+* NIL, it will return NIL.
+*/
+PRIVATE NODE* _bptree_leftmost(TREE* T) {
+	if (!T) { return NIL; }
+	NODE* leftmost = T->root;
+	while (leftmost && !(leftmost->leaf)) {
+		leftmost = leftmost->c[0];
+	}
+	return leftmost;
+}
+
+/**
 * @brief B+ tree traveral method.
 *
 * @param T B+ tree.
 * @param function traveral a element callback function.
 */
 PUBLIC void bptree_traveral(TREE* T, void function(ELEMENT)) {
-	_bptree_traveral(T->leftmost, function);
+	_bptree_traveral(_bptree_leftmost(T), function);
 }
 
 /////////////////////////////// TEST CASE //////////////////////////////////////
@@ -290,21 +406,21 @@ void printout_node(ELEMENT k) { printf("%d ", k); }
 * @brief test case for single insert, search and delete method.
 */
 void testcase_for_single() {
-/// 	TREE* T = bptree_create();
-/// 
-/// 	printf("bptree_insert(T, 32) = %d\n", bptree_insert(T, 32));
-/// 
-/// 	printf("bptree_search(T, 32) = %d\n", bptree_search(T, 32));
-/// 
-/// 	printf("bptree_delete(T, 32) = %d\n", bptree_delete(T, 32));
-/// 
-/// 	printf("bptree_search(T, 32) = %d\n", bptree_search(T, 32));
-/// 
-/// 	printf("bptree_traveral: ");
-/// 	bptree_traveral(T, printout_node);
-/// 	printf("\n");
-/// 
-/// 	bptree_destory(T);
+	TREE* T = bptree_create();
+
+	printf("bptree_insert(T, 32) = %d\n", bptree_insert(T, 32));
+
+	printf("bptree_search(T, 32) = %d\n", bptree_search(T, 32));
+
+	/// printf("bptree_delete(T, 32) = %d\n", bptree_delete(T, 32));
+
+	printf("bptree_search(T, 32) = %d\n", bptree_search(T, 32));
+
+	printf("bptree_traveral: ");
+	bptree_traveral(T, printout_node);
+	printf("\n");
+
+	bptree_destory(T);
 }
 
 #include <time.h>
@@ -313,33 +429,33 @@ void testcase_for_single() {
 * @brief test case for random number insert, search and delete.
 */
 void testcase_for_random() {
-/// 	TREE* T = bptree_create();
-/// 	int i = 0, n = 100;
-/// 	int _t = 0;
-/// 	srand(time(NULL));
-/// 	for (i = 0; i < n; i ++) {
-/// 		_t = rand() % n;
-/// 		printf("bptree_insert(T, %d) = %d\n", _t, bptree_insert(T, _t));
-/// 	}
-/// 	for (i = 0; i < n; i ++) {
-/// 		_t = rand() % n;
-/// 		printf("bptree_search(T, %d) = %d\n", _t, bptree_search(T, _t));
-/// 	}
-/// 
-/// 	printf("bptree_traveral: ");
-/// 	bptree_traveral(T, printout_node);
-/// 	printf("\n");
-/// 
-/// 	for (i = 0; i < n; i ++) {
-/// 		_t = rand() % n;
-/// 		printf("bptree_delete(T, %d) = %d\n", _t, bptree_delete(T, _t));
-/// 	}
-/// 
-/// 	printf("bptree_traveral: ");
-/// 	bptree_traveral(T, printout_node);
-/// 	printf("\n");
-/// 
-/// 	bptree_destory(T);
+	TREE* T = bptree_create();
+	int i = 0, n = 100;
+	int _t = 0;
+	srand(time(NULL));
+	for (i = 0; i < n; i ++) {
+		_t = rand() % n;
+		printf("bptree_insert(T, %d) = %d\n", _t, bptree_insert(T, _t));
+	}
+	for (i = 0; i < n; i ++) {
+		_t = rand() % n;
+		printf("bptree_search(T, %d) = %d\n", _t, bptree_search(T, _t));
+	}
+
+	printf("bptree_traveral: ");
+	bptree_traveral(T, printout_node);
+	printf("\n");
+
+//	for (i = 0; i < n; i ++) {
+//		_t = rand() % n;
+//		printf("bptree_delete(T, %d) = %d\n", _t, bptree_delete(T, _t));
+//	}
+//
+//	printf("bptree_traveral: ");
+//	bptree_traveral(T, printout_node);
+//	printf("\n");
+
+	bptree_destory(T);
 }
 
 int main(int argc, const char *argv[])
