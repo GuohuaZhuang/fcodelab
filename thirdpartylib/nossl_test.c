a9187cf5e927c29592f66c8a093c1d1e9f5f4976
firstboy0513 <guohua0594@sina.com>
2014-02-21 15:10:40 +0800

    nossl test case add BIO_should_retry and comments

--- thirdpartylib/nossl_test.c
+++ thirdpartylib/nossl_test.c
@@ -1,3 +1,29 @@
+/* Copyright (C) 
+* 2014 - firstboy0513
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+* 
+*/
+/**
+* @file nossl_test.c
+* @brief use libssl to implement nossl Connection.
+* reference:
+* 	http://www.ibm.com/developerworks/linux/library/l-openssl/index.html
+* @author firstboy0513
+* @version 0.0.1
+* @date 2014-02-21
+*/
 #include <openssl/ssl.h>
 #include <openssl/bio.h>
 #include <openssl/err.h>
@@ -5,6 +31,13 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+* @brief test case for nossl.
+* The START_BIO_READ goto label is use for BIO_should_retry function.
+*
+* @param host_and_port host_and_port string.
+* @param request request string.
+*/
 void testcase_nossl(char* host_and_port, const char* request) {
 	BIO* bio;
 	ERR_load_BIO_strings();
@@ -19,8 +52,12 @@ void testcase_nossl(char* host_and_port, const char* request) {
 	}
 	BIO_write(bio, request, strlen(request));
 	int p = 0; char r[1024] = {0};
+START_BIO_READ:
 	while ((p = BIO_read(bio, r, 1023)) > 0) {
 		r[p] = '\0'; printf("%s", r);
+	}
+	if (p < 0 && BIO_should_retry(bio)) {
+		goto START_BIO_READ;
 	}
 	BIO_free_all(bio);
 }
