d5405fc488f014e54186129e5f1b551e1bd5c576
firstboy0513 <guohua0594@sina.com>
2014-03-06 11:42:13 +0800

    Pacman game initialize table

--- mathproblem/pacman.c
+++ mathproblem/pacman.c
@@ -104,7 +104,10 @@
 * 		-1 -1 -1
 * 	Output:
 * 		No path
-*
+* 
+* Simple algorithm:
+* 	1. generate a table has rows*columns capacity, and with passed, road, weight.
+* 	2. 
 *
 * @author firstboy0513
 * @version 0.0.1
@@ -113,7 +116,127 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct _road_element {
+//	int x;			// coordinate x-axis
+//	int y;			// coordinate y-axis
+	struct _road_point point;		// road point
+	int road;		// path length
+	int weight;		// path weight sum
+	struct _road_element* paths;	// path from starting
+} ROAD_ELE;
+
+typedef struct _road_point {
+	int x;
+	int y;
+} ROAD_POINT;
+
+typedef struct _table_cell {
+	int score;		// score of fruit
+	int passed;		// passed or not
+} TABLE_CELL;
+
+typedef struct _table {
+	TABLE_CELL** arr;
+	ROAD_POINT start;
+	ROAD_POINT end;
+	int rows;
+	int columns;
+} TABLE;
+
+TABLE* init_table(int rows, int columns, 
+	int start_x, int start_y, int end_x, int end_y) {
+	if (rows < 1 || columns < 1) {
+		fprintf("initialize table failed\n"); exit(-1);
+	}
+	if (start_x < 1 || start_x > rows || start_y < 1 || start_y > columns
+		|| end_x < 1 || end_x > rows || end_y < 1 || end_y > columns) {
+		fprintf("initialize table failed\n"); exit(-1);
+	}
+	TABLE* table = (TABLE*) malloc(sizeof(TABLE));
+	if (!table) { fprintf(stderr, "malloc error\n"); exit(-1); }
+	int i = 0, j = 0;
+	table->arr = (TABLE_CELL**) malloc(sizeof(TABLE_CELL*) * rows);
+	if (!table->arr) { fprintf(stderr, "malloc error\n"); exit(-1); }
+	for (i = 0; i < rows; i ++) {
+		table->arr[i] = (TABLE_CELL*) malloc(sizeof(TABLE_CELL) * columns);
+		if (!table->arr[i]) { fprintf(stderr, "malloc error\n"); exit(-1); }
+		for (j = 0; j < columns; j ++) {
+			table->arr[i][j] = 1;
+		}
+	}
+	table->rows = rows; table->columns = columns;
+	table->start.x = start_x; table->start.y = start_y;
+	table->end.x = end_x; table->end.y;
+	return table;
+}
+
+void release_table(TABLE* table) {
+	int i = 0;
+	for (i = 0; i < table->rows; i ++) {
+		free(table->arr[i]);
+	}
+	free(table->arr);
+	free(table);
+}
+
+void setweight_table(TABLE* table, int x, int y, int weight) {
+	if (x > 0 && x <= rows && y > 0 && y <= columns) {
+		table->arr[x][y] = weight;
+	} else {
+		fprintf(stderr, "set weight failed!\n"); exit(-1);
+	}
+}
+
+void add_four_direction(ROAD_ELE s, forward_list) {
+	if (... is updated) LIST_ADD(forward_list, s->left);
+	if (... is updated) LIST_ADD(forward_list, s->top);
+	if (... is updated) LIST_ADD(forward_list, s->right);
+	if (... is updated) LIST_ADD(forward_list, s->bottom);
+}
+
+ROAD_ELE* operator_forward_list(forward_list) {
+	ROAD_ELE* new_list;
+	while (forward_list) {
+		do {
+			ROAD_ELE t = LIST_POP(forward_list);
+			add_four_direction(, new_list);
+		} while (forward_list);
+	}
+	return new_list;
+}
+
+ROAD_ELE pacman_shortestpath(TABLE table) {
+	add_four_direction(start, forward_list);
+	while (forward_list) {
+		forward_list = operator_forward_list(forward_list);
+	}
+	if (ending->passed) {
+		printf("printf the paths\n");
+	} else {
+		printf("No path\n");
+	}
+}
+
 int main(int argc, const char *argv[])
 {
+	printf("Input:\n");
+	int rows = 0, columns = 0, start_x = 0, start_y = 0, end_x = 0, end_y = 0;
+	scanf("%d", &rows);
+	scanf("%d", &rows); scanf("%d", &columns);
+	scanf("%d", &start_x); scanf("%d", &start_y);
+	scanf("%d", &end_x); scanf("%d", &end_y);
+	TABLE* table = init_table(rows, columns, start_x, start_y, end_x, end_y);
+	int x = 0, y = 0, w = 0;
+	do {
+		scanf("%d", &x); scanf("%d", &y); scanf("%d", &w);
+		if (-1 == x || -1 == y) { break; }
+		setweight_table(table, x, y, w);
+	}
+
+	printf("Output:\n");
+	pacman_shortestpath(table);
+
+	release_table(table);
+
 	return 0;
 }
